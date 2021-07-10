5ddddcbf5b080f9be2a1d4f9065300cf6584abc9
cristina <cristina.outeda.rua@gmail.com>
2021-07-10 19:00:10 +0200

    created stage component

---
+++ src/components/Stage/Stage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyledStage } from "./Style";
+import { Cell } from "./../Cell";
+
+export function Stage({ stage }) {
+  return (
+    <StyledStage width={stage[0].length} height={stage.length}>
+      {stage.map((row) =>
+        row.map((cell, i) => <Cell key={i} type={cell[0]} />)
+      )}
+    </StyledStage>
+  );
+}
