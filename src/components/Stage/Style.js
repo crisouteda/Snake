5c3390054f856624d51af1b385d25a46d42096be
cristina <cristina.outeda.rua@gmail.com>
2021-07-10 19:47:21 +0200

    updated stage style

--- src/components/Stage/Style.js
+++ src/components/Stage/Style.js
@@ -4,13 +4,8 @@ export const StyledStage = styled.div`
   display: grid;
   grid-template-rows: repeat(
     ${(props) => props.height},
-    calc(25vw / ${(props) => props.width})
+    calc(550px / ${(props) => props.width})
   );
-  grid-template-columns: repeat(${(props) => props.width}, 1fr);
-  grid-gap: 1px;
-  border: 2px solid #333;
-  width: 100%;
-  max-width: 25vw;
-  background: #111;
-  margin: 0 20px 0 20px;
+  grid-template-columns: repeat(${(props) => props.width}, 0fr);
+  width: 550px;
 `;
