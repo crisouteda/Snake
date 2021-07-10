fe80cd9ff1dfdde83b792981ad82d51547cabf3d
cristina <cristina.outeda.rua@gmail.com>
2021-07-10 19:01:24 +0200

    useStage hook created

---
+++ src/hooks/useStage.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { createStage } from "../helpers";
+
+export const useStage = () => {
+  const [stage, setStage] = useState(createStage());
+
+  return [stage, setStage];
+};
