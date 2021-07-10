f1d470514c1786ed4fc0a39c6af802e92b14f4b3
cristina <cristina.outeda.rua@gmail.com>
2021-07-10 19:46:41 +0200

    added cell types to cell component

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyledCell } from "./Style";
+import { TYPES } from "../../helpers";
 
 export function Cell({ type }) {
-  return <StyledCell type={type}></StyledCell>;
+  return <StyledCell type={type} color={TYPES[type].color}></StyledCell>;
 }
